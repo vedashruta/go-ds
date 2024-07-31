d519cf0e6dc8c6c7f0978e3b1fbe32616c09dce0
vedashruta <vedashrutabhat@gmail.com>
2024-07-31 21:10:29 +0530

    [MNT] Added Stack Documentation [MNT] Added Queue Documentation

--- queue/arrayqueue/arrayqueue.go
+++ queue/arrayqueue/arrayqueue.go
@@ -11,15 +11,20 @@ type Queue[V comparable] struct {
 	arrayList *arraylist.ArrayList[V]
 }
 
+// Creates a new arrayList of type Queue
 func New[V comparable]() (q *Queue[V]) {
 	q = &Queue[V]{arrayList: arraylist.New[V]()}
 	return
 }
 
+// Adds the element to the end of the arrayList
+// i.e in an arrayList of size n,element will be inserted to n+1th position
 func (q *Queue[V]) Enqueue(value V) {
 	q.arrayList.Add(value)
 }
 
+// Deleted the element that was inserted first
+// i.e in an arrayList of size n,element will be deleted from ith position where i<=n
 func (q *Queue[V]) Dequeue() (value V, ok bool) {
 	value, ok = q.arrayList.Get(0)
 	if ok {
@@ -28,6 +33,8 @@ func (q *Queue[V]) Dequeue() (value V, ok bool) {
 	return
 }
 
+// Returns the first element that was inserted
+// i.e in an arrayList of size n,element from ith position will be returned where i<=n
 func (q *Queue[V]) Peek() (value V, ok bool) {
 	value, ok = q.arrayList.Get(0)
 	if ok {
--- stack/arrayStack/arrayStack.go
+++ stack/arrayStack/arrayStack.go
@@ -11,15 +11,20 @@ type Stack[V comparable] struct {
 	arrayList *arraylist.ArrayList[V]
 }
 
+// Creates a new arrayList of type Stack
 func New[V comparable]() (stack *Stack[V]) {
 	stack = &Stack[V]{arrayList: arraylist.New[V]()}
 	return
 }
 
+// Adds the element to the end of the arrayList
+// i.e in an arrayList of size n,element will be inserted to n+1th position
 func (s *Stack[V]) Push(element V) {
 	s.arrayList.Add(element)
 }
 
+// Removes the last element that was inserted
+// i.e in an arrayList of size n,element will be deleted from n-1th position
 func (s *Stack[V]) Pop() (element V, ok bool) {
 	element, ok = s.arrayList.Get(s.arrayList.Size() - 1)
 	if ok {
@@ -29,6 +34,8 @@ func (s *Stack[V]) Pop() (element V, ok bool) {
 	return
 }
 
+// Returns the last element that was inserted
+// i.e in an arrayList of size n,element from n-1th position will be returned
 func (s *Stack[V]) Peek() (element V, ok bool) {
 	element, ok = s.arrayList.Get(s.arrayList.Size() - 1)
 	if ok {
